123e7c4f79b4a92d7f2541e51dd3607bf3452e2e
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-25 12:47:59 +1000

    Adding the viewlet manager for the encouragement area.

---
+++ gs/group/encouragement/interfaces.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+from zope.viewlet.interfaces import IViewletManager
+
+class IGroupEncouragement(IViewletManager):
+    '''A viewlet manager for encouragement on the group homepage'''
+
