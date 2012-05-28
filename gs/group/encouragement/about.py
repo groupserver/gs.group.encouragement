688d9296b5e16d6c59555caced16d28cba70d3f4
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-28 16:05:35 +1000

    Adding the 'Write an About' encouragement.

---
+++ gs/group/encouragement/about.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from gs.group.privacy.visibility import GroupVisibility
+from invite import Invite
+
+class About(Invite):
+    @Lazy
+    def hasAboutText(self):
+        t = getattr(self.context, 'aboutText', '').strip()
+        retval = bool(t)
+        return retval
+
+    @property
+    def show(self):
+        retval = ((not self.hasAboutText)
+            and (self.statsQuery.posts_per_day(self.groupInfo.id) != [])
+            and (self.memberCount > 1))
+        assert type(retval) == bool
+        return retval
+
