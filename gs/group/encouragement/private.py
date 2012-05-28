bf440b315c3ad65fdad0996f759c725bfcfb0680
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-28 11:53:51 +1000

    Adding a Private class.
    --HG--
    rename : gs/group/encouragement/browser/templates/encourageprivate.pt => gs/group/encouragement/browser/templates/private.pt

---
+++ gs/group/encouragement/private.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from Products.GSGroupMember.groupMembersInfo import GSGroupMembersInfo
+from invite import Invite
+
+class Private(Invite):
+    pass
+
