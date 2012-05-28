5afc901b62f404b30877d42204d74b06ac15b739
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-28 13:47:14 +1000

    Adding the test for the secret group.

--- gs/group/encouragement/private.py
+++ gs/group/encouragement/private.py
@@ -1,12 +1,26 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
+from gs.group.privacy.visibility import GroupVisibility
 from invite import Invite
 
+# TODO: change to an option. 
+MAX_SECRET_MEMBERS = 11
+# --=mpj17=--
+# The number of secret members was set to 11 by dvr and mpj17. It seemed
+#   a good idea at the time.
+
 class Private(Invite):
+    @Lazy
+    def isSecret(self):
+        gv = GroupVisibility(self.groupInfo)
+        return gv.isSecret
+        
     @property
     def show(self):
         ppd = self.statsQuery.posts_per_day(self.groupInfo.id)
-        retval = (ppd != []) and (self.memberCount > 11) # And secret
+        retval = ((ppd != []) 
+            and self.isSecret
+            and (self.memberCount > MAX_SECRET_MEMBERS))
         assert type(retval) == bool
         return retval
 
