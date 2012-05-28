22c574495d0133751378924e97ebbbba3320799b
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-28 11:22:17 +1000

    Only show the Sart Topic text when there are not topics.

--- gs/group/encouragement/starttopic.py
+++ gs/group/encouragement/starttopic.py
@@ -2,6 +2,8 @@
 from zope.cachedescriptors.property import Lazy
 from zope.component import getMultiAdapter
 from Products.GSGroup.interfaces import IGSMailingListInfo
+from Products.GSParticipationStats.queries import MessageQuery as \
+    StatsQuery
 from gs.group.member.canpost.interfaces import IGSPostingUser
 from gs.group.home.simpletab import UserInfoTab
 
@@ -19,9 +21,17 @@ class StartTopic(UserInfoTab):
         retval = l.get_property('mailto')
         return retval
 
+    @Lazy
+    def statsQuery(self):
+        da = self.context.zsqlalchemy
+        retval = StatsQuery(self.context, da)
+        assert retval
+        return retval
+
     @property
     def show(self):
-        retval = self.canPost.canPost 
+        ppd = self.statsQuery.posts_per_day(self.groupInfo.id)
+        retval = self.canPost.canPost and (ppd == [])
         assert type(retval) == bool
         return retval
 
