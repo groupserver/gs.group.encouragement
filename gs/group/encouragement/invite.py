b52c335f4b239536e0db3fb0ee8b09abee51a6cf
Michael JasonSmith <mpj17@onlinegroups.net>
2012-05-28 11:43:29 +1000

    Showing the Invite Another Member encouragement at the right time.

--- gs/group/encouragement/invite.py
+++ gs/group/encouragement/invite.py
@@ -1,12 +1,28 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
-from zope.component import getMultiAdapter
-from Products.GSGroup.interfaces import IGSMailingListInfo
-from Products.GSParticipationStats.queries import MessageQuery as \
-    StatsQuery
-from gs.group.member.canpost.interfaces import IGSPostingUser
-from gs.group.home.simpletab import UserInfoTab
+from Products.GSGroupMember.groupMembersInfo import GSGroupMembersInfo
+from starttopic import StartTopic
 
-class Invite(UserInfoTab):
-    pass
+class Invite(StartTopic):
+    # See the "show" property for why "Invite" inherits from
+    #   "StartTopic"
+    @Lazy
+    def membersInfo(self):
+        retval = GSGroupMembersInfo(self.group)
+        assert retval
+        return retval
+
+    @Lazy
+    def memberCount(self):
+        retval = self.membersInfo.fullMemberCount
+        return retval
+        
+    @property
+    def show(self):
+        # Only show the Invite encouragement when the Topic 
+        #   encouragement is not shown.
+        ppd = self.statsQuery.posts_per_day(self.groupInfo.id)
+        retval = (ppd != []) and self.memberCount < 2
+        assert type(retval) == bool
+        return retval
 
