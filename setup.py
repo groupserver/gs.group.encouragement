6d631ee099fe6019180e4ee931600e5a158321a7
Michael JasonSmith <mpj17@onlinegroups.net>
2013-10-01 16:59:01 -0700

    Following the StatsQuery to gs.group.stats

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -41,10 +54,10 @@ setup(name='gs.group.encouragement',
         'gs.group.home',
         'gs.group.member.canpost',
         'gs.group.privacy',
+        'gs.group.stats',
         'gs.viewlet',
         'Products.GSGroup',
         'Products.GSGroupMember',
-        'Products.GSParticipationStats',
     ],
     entry_points="""
     # -*- Entry points: -*-
