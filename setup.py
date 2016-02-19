238c2b5ecc93876f725047d2371cedf04f7144e4
Michael JasonSmith <mpj17@onlinegroups.net>
2016-02-19 09:52:01 -0800

    Updating the product metadata

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ############################################################################
 #
-# Copyright © 2012, 2013, 2014, 2015 OnlineGroups.net and Contributors.
+# Copyright © 2012, 2013, 2014, 2015, 2016 OnlineGroups.net and Contributors.
 #
 # All Rights Reserved.
 #
@@ -18,6 +18,7 @@ import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.group.encouragement'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
@@ -27,7 +28,7 @@ with codecs.open(os.path.join("docs", "HISTORY.rst"),
     long_description += '\n' + f.read()
 
 setup(
-    name='gs.group.encouragement',
+    name=name,
     version=version,
     description="Encouragement to the administrator of a GroupServer group",
     long_description=long_description,
@@ -46,10 +47,11 @@ setup(
     keywords='group, groupserver, member, admin, group admin',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
-    url='https://github.com/groupserver/gs.group.encouragement',
+    url='https://github.com/groupserver/{0}'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group'],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
@@ -64,12 +66,11 @@ setup(
         'gs.content.js.bootstrap',
         'gs.group.base',
         'gs.group.home',
+        'gs.group.member.base',
         'gs.group.member.canpost',
         'gs.group.member.viewlet',
         'gs.group.privacy',
         'gs.group.stats',
-        'Products.GSGroup',
-        'Products.GSGroupMember',
     ],
     entry_points="""
     # -*- Entry points: -*-
